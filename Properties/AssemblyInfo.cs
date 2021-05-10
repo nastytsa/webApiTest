9dd6da3189872faa8b36efd1e2a0a3b07af420d0
nastytsa <puppiduppi0@gmail.com>
2021-05-10 14:25:17 +0300

    initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("weApiTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("weApiTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6f298a06-b385-4568-8d11-39077686219c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Json;
+using System.Net;
+using NUnit.Framework;
+using RA;
+
+namespace weApiTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private string ACCESS_TOKEN = "k22xIkOEk4UAAAAAAAAAAQGnV7_UHGw6QPS6c49018sc7SeM3fSdgjytRrq0HVff";
+
+        [TestMethod]
+        public void A_uploadTest()
+        {
+
+            JsonObject json = new JsonObject();
+            json.Add("mode", "add");
+            json.Add("autorename", true);
+            json.Add("path", "/testing.json");
+
+            JsonObject jsonFile = new JsonObject();
+            jsonFile.Add("message", "Hello");
+
+            new RestAssured()
+                .Given()
+                    .Header("Dropbox-API-Arg", json.ToString())
+                    .Header("Content-Type", "application/json")
+                    .Header("Authorization", "Bearer " + ACCESS_TOKEN)
+                    .Body(jsonFile.ToString())
+                .When()
+                    .Post("https://content.dropboxapi.com/2/files/upload")
+                .Then()
+                    .TestStatus("test a", x => x == (int)HttpStatusCode.OK)
+                    .Assert("test a");
+        }
+        [TestMethod]
+        public void B_getMetadataTest()
+        {
+
+            JsonObject json = new JsonObject();
+            json.Add("path", "/testing.txt");
+
+            new RestAssured()
+                .Given()
+                .Header("Authorization", "Bearer " + ACCESS_TOKEN)
+                .Header("Content-Type", "application/json")
+                .Body(json.ToString())
+                .When()
+                .Post("https://api.dropboxapi.com/2/files/get_metadata")
+                .Then()
+                .TestStatus("test a", x => x == (int)HttpStatusCode.OK)
+                .Assert("test a");
+        }
+
+        [TestMethod]
+        public void C_deleteTest()
+        {
+
+            JsonObject json = new JsonObject();
+            json.Add("path", "/testing.txt");
+
+            new RestAssured()
+                .Given()
+                    .Header("Authorization", "Bearer " + ACCESS_TOKEN)
+                    .Header("Content-Type", "application/json")
+                    .Body(json.ToString())
+                .When()
+                    .Post("https://api.dropboxapi.com/2/files/delete_v2")
+                .Then()
+                    .TestStatus("test a", x => x == (int)HttpStatusCode.OK)
+                    .Assert("test a");
+        }
+    }
+}
